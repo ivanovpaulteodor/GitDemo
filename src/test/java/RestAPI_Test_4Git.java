d854cf6acb1564fecc41f95701de563c4c787948
ivanovpaulteodor <PaulTeodor.Ivanov@endava.com>
2020-08-05 13:08:17 +0300

    This is commit from GitDemo different time zone, added RestAPI class

---
+++ src/test/java/RestAPI_Test_4Git.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class RestAPI_Test_4Git {
+
+    @Test
+    public void postJira(){
+        System.out.println("postJira1");
+    }
+
+    @Test
+    public void deleteTwitter(){
+        System.out.println("deleteTwitter");
+    }
+
+}
